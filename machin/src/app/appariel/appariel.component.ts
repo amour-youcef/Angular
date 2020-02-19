b9467672aa430e3539d797d192394aa92524731e
amour-youcef <amour-youcef@hotmail.com>
2020-02-19 14:22:19 +0100

    maj

--- machin/src/app/appariel/appariel.component.ts
+++ machin/src/app/appariel/appariel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AppareilService } from '../services/appreil.service';
 
 @Component({
   selector: 'app-appariel',
@@ -6,17 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appariel.component.css']
 })
 export class ApparielComponent implements OnInit {
-  
-  appareilName ='Machin a lave ';
-  appareilStatus = 'eteint' ;
 
-  constructor() { }
+  @Input() appareilName: String;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+  @Input() index: number;
+  @Input() id: number;
+
+  constructor(private appareilService: AppareilService) { }
 
   ngOnInit(): void {
   }
- 
-  getStatus(){
+
+  getStatus() {
     return this.appareilStatus;
   }
-   
+  getColor() {
+    if (this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if (this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
+
+  onSwitchOn() {
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+  onSwitchOff() {
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }
 }
---
+++ machin/src/app/services/appreil.service.ts
@@ -0,0 +1,92 @@
+import { Subject } from 'rxjs';
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class AppareilService {
+  appareilsSubject = new Subject<any[]>(); 
+  private appareils = [];
+   
+    constructor(private httpClient: HttpClient){
+
+    }
+
+    emitAppareilSubject() {
+      this.appareilsSubject.next(this.appareils.slice());
+    }
+
+    getAppareilById(id: number) {
+        const appareil = this.appareils.find(
+          (s) => {
+            return s.id === id;
+          }
+        );
+        return appareil;
+    }
+
+    switchOnAll() {
+      for(let appareil of this.appareils) {
+        appareil.status = 'allumé';
+      }
+      this.emitAppareilSubject();
+  }
+  
+  switchOffAll() {
+      for(let appareil of this.appareils) {
+        appareil.status = 'éteint';
+        this.emitAppareilSubject();
+      }
+  }
+  
+  switchOnOne(i: number) {
+      this.appareils[i].status = 'allumé';
+      this.emitAppareilSubject();
+  }
+  
+  switchOffOne(i: number) {
+      this.appareils[i].status = 'éteint';
+      this.emitAppareilSubject();
+  }
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name: '',
+      status: ''
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+    this.appareils.push(appareilObject);
+    this.emitAppareilSubject();
+}
+
+saveAppareilsToServer(){
+  this.httpClient
+  .put('https://http-client-demo-da44e.firebaseio.com/appareils.json', this.appareils)
+  .subscribe(
+    ()=>{
+      console.log('Enregistrement terminé !');
+    },
+    (error)=>{
+      console.log('Erreur de sauvegarde !' +error);
+      
+    }
+  )
+}
+
+getAppareilsFromServer(){
+  this.httpClient
+    .get<any[]>('https://http-client-demo-da44e.firebaseio.com/appareils.json')
+    .subscribe(
+      (response) => {
+        this.appareils = response;
+        this.emitAppareilSubject();
+      }, (error) => {
+        console.log('Erreur de chargement ! ' + error);
+        
+      }
+    );
+}
+
+}
