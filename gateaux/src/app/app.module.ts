9b81585d792216e603650a648a7e8d789c2d6ca5
dev <amour-youcef@hotmail.com>
2020-03-12 15:09:12 +0100

    maj

--- gateaux/src/app/app.module.ts
+++ gateaux/src/app/app.module.ts
@@ -23,6 +23,10 @@ import { CommanderComponent } from './components/commander/commander.component';
 import { LoginComponent } from './components/login/login.component';
 import { PhotosComponent } from './components/photos/photos.component';
 import { Home1Component } from './components/home1/home1.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { FooterComponent } from './components/footer/footer.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +38,8 @@ import { Home1Component } from './components/home1/home1.component';
     CommanderComponent,
     LoginComponent,
     PhotosComponent,
-    Home1Component
+    Home1Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -47,8 +52,9 @@ import { Home1Component } from './components/home1/home1.component';
     FlexLayoutModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule
-   
+    MatCardModule,
+    CarouselModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- gateaux/src/app/components/commander/commander.component.ts
+++ gateaux/src/app/components/commander/commander.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-commander',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commander.component.scss']
 })
 export class CommanderComponent implements OnInit {
+ 
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
 
   constructor() { }
 
@@ -13,3 +39,4 @@ export class CommanderComponent implements OnInit {
   }
 
 }
+
