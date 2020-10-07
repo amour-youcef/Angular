0fc4d91b69887be0d80577d6b2c3bd807f7a4426
amour-youcef <amour-youcef@hotmail.com>
2020-10-07 11:28:11 +0200

    07102020

--- gateaux/src/app/components/interfaces/good.interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface Good {
-
-    name?: string ,
-    price?: number ,
-    photoUrl?: string
-    
-}
--- gateaux/src/app/material/material.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-
-const materials = [MatButtonModule]
-@NgModule({
-  imports: [materials],
-  exports: [materials]
-})
-export class MaterialModule { }
