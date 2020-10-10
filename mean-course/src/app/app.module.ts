96e78ee8dc1467106dc24bb988b176f32ed56a0f
amour-youcef <amour-youcef@hotmail.com>
2020-10-10 18:03:31 +0200

    10102020

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PosteComponent } from './postes/poste/poste.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PosteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
